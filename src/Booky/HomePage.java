5635030df96a6129d14c3e03fe78f11c5d211267
gabgab <gabgab@10.85.60.79>
2018-11-09 14:11:02 -0800

    Update: just to commit

--- src/Booky/HomePage.java
+++ src/Booky/HomePage.java
@@ -29,7 +29,7 @@ public class HomePage extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		System.out.println("Herrro");
 	}
 
 }
